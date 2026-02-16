90550ca5244efb8632505b2affdd5d425ebbd8d4
9mrk9 <91804294+9mrk9@users.noreply.github.com>
2026-02-16 19:00:08 +0000

    fix: add update-toy docs

--- src/toys/dto/update-toy.dto.ts
+++ src/toys/dto/update-toy.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiSchema, PartialType } from '@nestjs/swagger';
 import { CreateToyDto } from './create-toy.dto';
 
+@ApiSchema({
+  name: 'UpdateToyDto',
+  description: 'Data transfer object for updating a toy',
+})
 export class UpdateToyDto extends PartialType(CreateToyDto) {}
